a8428bed279e01c7dfe683a5576bb7279411b698
Andrew <a.tupko98>
2026-04-23 16:26:50 +0200

    develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+const BigImage = document.querySelector('#largeImg');
+const Thumbs = document.querySelector('thumbs');
+Thumbs.addEventListener(click, (e) => 
+const link = e.target.closest('a'));
+
+if (!link) return
+
+e.PreventDefault();
+
+BigImage.src = link.href;
